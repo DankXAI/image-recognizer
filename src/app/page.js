52756995b0869a2e7cf84cd1a0527435f1386885
DankXAI <dankxai101@gmail.com>
2025-02-28 15:10:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import ImageUploaderContainer from "@/components/image-uploader-container";
 export default function Home() {
   return (
     <div className="flex flex-col items-center h-full">
-      <header className="flex items-center justify-center">
+      <header className="sm:flex items-center justify-center">
         <img
           src="/images/zepto-logo.png"
           alt="logo"
-          className="w-[150px] h-28 object-cover absolute left-0"
+          className="sm:w-[150px] w-[120px] h-28 object-cover sm:absolute left-0 mx-auto sm:mx-0"
         />
         <Header />
       </header>
